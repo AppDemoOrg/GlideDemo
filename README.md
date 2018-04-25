de1badf9cd7b7d9c4db3c5a075353f767aad5657
hwq <weiqi.huang@pisofttech.com>
2018-04-25 15:20:29 +0800

    整理代码

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # GlideDemo
-Glide with circle and round imageview
 
 使用Glide加载圆形或者圆角的图片，其中圆角的弧度可以自定义
--- app/src/androidTest/java/cn/xm/weidongjian/glide/ApplicationTest.java
+++ app/src/androidTest/java/cn/xm/weidongjian/glide/ApplicationTest.java
@@ -1,4 +1,4 @@
-package cn.xm.weidongjian.glidedemo;
+package cn.xm.weidongjian.glide;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
